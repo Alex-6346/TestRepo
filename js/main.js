093f49b739e9f2afbbbd5900a2ea753c542de3a5
Alex Kotkov <oleksandr.kotkov@ukma.edu.ua>
2020-10-10 16:15:54 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+console.log('Testing on 3d practice..');
+
+
+
+
+window.onload = function() {
+alert('no way');
+var square2=document.getElementById('square-2');
+square2.style.backgroundColor='violet';
+
+}
+
+$ (document).ready(function () {
+
+$('#square-4').css('background-color','brown'); 
+
+
+});
